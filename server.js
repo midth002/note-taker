f5a779187ef27bae16c1c848e101ed8efb036d64
Andrew Midthun <midth002@gmail.com>
2022-04-20 16:26:34 -0500

    Added routes folder and get routes.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-// const apiRoutes = require('./routes/apiRoutes');
+const path = require('path')
+const api = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api)
+
 app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'public/index.html')))
+
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
+
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
